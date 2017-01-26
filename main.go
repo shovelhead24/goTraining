5012ff31ab9f76bd6af1d9a7cab27d863c9066b0
Declan McGibney <dmcgibney@live.ie>
2017-01-26 13:34:41 +0000

    section review computer fundamentals

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello Declan!")
-	fmt.Println("hello Declan!")
+	for i := 30; i < 250; i++ {
+		//	fmt.Println("hello Declan!")
+		//	fmt.Println("hello Declan!")
+		//fmt.Println(42)
+		//fmt.Printf("%d - %b \n", 42, 42)
+		fmt.Printf("%d \t %b \t %X \t %q \n", i, i, i, i)
+	}
 }
